553b3f6536426cdeb9f4342fefc6eb24a8fe1b3f
Amy-Car20 <amycaruana2000@gmail.com>
2024-11-01 15:20:55 +0100

    Updated index.js

--- Projects/Vue.js-Project/src/firebase/index.js
+++ Projects/Vue.js-Project/src/firebase/index.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD_Vzc-Ft_bM9cCojsvgJEar8Awif6NAh8",
+  apiKey: "",
   authDomain: "bookstoreassigment.firebaseapp.com",
   projectId: "bookstoreassigment",
   storageBucket: "bookstoreassigment.appspot.com",
-  messagingSenderId: "255485527508",
-  appId: "1:255485527508:web:42cab2258d38a4cc3a8241",
-  measurementId: "G-3PPVYSY37H"
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
